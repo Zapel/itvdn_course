08c6ec556102fef7c61a43b4268a68b923df9c14
Zapel <lazebaoleg@gmail.com>
2019-05-31 06:42:24 +0300

    add lesson5

---
+++ lesson5/tests.py
@@ -0,0 +1,48 @@
+import unittest
+import lesson5.cbr_api as cbr_api
+from lesson5 import test_api
+from lesson5 import models
+from lesson5 import privat_api
+
+
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        models.init_db()
+
+    def test_main(self):
+        xrate = models.XRate.get(id=1)
+        updated_before = xrate.updated
+        self.assertEqual(xrate.rate, 1.0)
+        test_api.update_xrates(840, 980)
+        xrate = models.XRate.get(id=1)
+        update_after = xrate.updated
+
+        self.assertEqual(xrate.rate, 1.01)
+        self.assertGreater(update_after, updated_before)
+
+    def test_privat(self):
+        xrate = models.XRate.get(id=1)
+        updated_before = xrate.updated
+        self.assertEqual(xrate.rate, 1.0)
+        privat_api.update_xrates(840, 980)
+        xrate = models.XRate.get(id=1)
+        update_after = xrate.updated
+
+        self.assertGreater(xrate.rate, 25)
+        self.assertGreater(update_after, updated_before)
+
+    def test_cdr(self):
+        xrate = models.XRate.get(from_currency=840, to_currency=643)
+        update_before = xrate.updated
+        self.assertEqual(xrate.rate, 1.0)
+        cbr_api.update_xrates(840, 643)
+        xrate = models.XRate.get(from_currency=840, to_currency=643)
+        updated_after = xrate.updated
+
+        self.assertGreater(xrate.rate, 60)
+        self.assertGreater(updated_after, update_before)
+
+    if __name__ == '__main__':
+        unittest.main()
+
